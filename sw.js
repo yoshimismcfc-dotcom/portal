467a0b0e260fd3084449c71680eeb09178e03ce5
yoshimismcfc-dotcom <portal@yoshimi-smc>
2026-07-08 23:39:34 +0000

    LINEコピー統一：全ページ緑ボタン化・昼食休憩toMins順序バグ修正・対戦表ボタン整備

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker - 自動キャッシュ無効化
-// バージョン: 20260705014404
+// バージョン: 20260708233928
 
-const CACHE_VERSION = '20260705014404';
+const CACHE_VERSION = '20260708233928';
 
 // インストール：即座にアクティベート
 self.addEventListener('install', function(e){
