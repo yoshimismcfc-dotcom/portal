131c1859a0ecaa18f3ad94cb8de8716b2b9d4358
yoshimismcfc-dotcom <yoshimi.smc.fc@gmail.com>
2026-07-10 20:02:21 +0000

    天気WBGT：manual-area/manual-placeをHTMLに追加・nullチェック完全対応

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker - 自動更新対応版
-// バージョン: 20260710223836
+// バージョン: 20260710200217
 
-const CACHE_VERSION = '20260710223836';
+const CACHE_VERSION = '20260710200217';
 const CACHE_NAME = 'smc-portal-' + CACHE_VERSION;
 
 // インストール：新バージョンを即座にアクティベート
