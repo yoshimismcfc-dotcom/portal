f4e09a01e54a054d2d6cbd01d39bf0c9b1073046
yoshimismcfc-dotcom <yoshimi.smc.fc@gmail.com>
2026-07-11 06:10:12 +0900

    自動アラート更新用にキャッシュバージョンを更新

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker - 自動更新対応版
-// バージョン: 20260711052840
+// バージョン: 20260711055441
 
-const CACHE_VERSION = '20260711052840';
+const CACHE_VERSION = '20260711055441';
 const CACHE_NAME = 'smc-portal-' + CACHE_VERSION;
 
 // インストール：新バージョンを即座にアクティベート
