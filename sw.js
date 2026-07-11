bf51b836cc1a30afdef95d907c78a130a87ee040
yoshimismcfc-dotcom <yoshimi.smc.fc@gmail.com>
2026-07-11 07:55:27 +0000

    試合調整：タップ編集モーダルと関数(openEditDate/saveEditDate/deleteEditDate)を追加

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker - 自動更新対応版
-// バージョン: 20260711055441
+// バージョン: 20260711075522
 
-const CACHE_VERSION = '20260711055441';
+const CACHE_VERSION = '20260711075522';
 const CACHE_NAME = 'smc-portal-' + CACHE_VERSION;
 
 // インストール：新バージョンを即座にアクティベート
