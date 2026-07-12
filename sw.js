41a9e8f2fe44b6ced1c6bc6b3103462fff5510fc
yoshimismcfc-dotcom <yoshimi.smc.fc@gmail.com>
2026-07-12 07:37:30 +0000

    試合調整：Firebase空データ・壊れたデータへの完全防御と自動復元（表が空になる問題を修正）

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker - 自動更新対応版
-// バージョン: 20260712073303
+// バージョン: 20260712073723
 
-const CACHE_VERSION = '20260712073303';
+const CACHE_VERSION = '20260712073723';
 const CACHE_NAME = 'smc-portal-' + CACHE_VERSION;
 
 // インストール：新バージョンを即座にアクティベート
