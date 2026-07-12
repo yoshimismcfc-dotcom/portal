7e5900a7aa5a7094e30a8f5358ee525705678fe5
yoshimismcfc-dotcom <yoshimi.smc.fc@gmail.com>
2026-07-12 07:33:08 +0000

    試合調整：端末に残る実データをクラウドへ自動復元するロジック追加

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker - 自動更新対応版
-// バージョン: 20260712071949
+// バージョン: 20260712073303
 
-const CACHE_VERSION = '20260712071949';
+const CACHE_VERSION = '20260712073303';
 const CACHE_NAME = 'smc-portal-' + CACHE_VERSION;
 
 // インストール：新バージョンを即座にアクティベート
