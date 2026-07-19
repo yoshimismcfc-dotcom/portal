270e17c89de1856e686b49f5adf3b6dbe40274e1
yoshimismcfc-dotcom <yoshimi.smc.fc@gmail.com>
2026-07-19 21:34:19 +0900

    試合調整で来年の日程選択が今年へ戻る不具合を修正
    * fix: 日程編集で保存済みの年を維持
    
    * fix: 年をまたぐ日程選択を日程IDで保持
    
    * fix: 保存済みISO日付の判定を安定化
    
    * docs: 年をまたぐ日程選択の保持を追記
    
    * test: 複数年度の日程選択と年保持を監査
    
    * chore: PWAバージョンを20260719-9へ更新
    
    * chore: PWAバージョンを20260719-9へ更新
    
    * chore: PWAバージョンを20260719-9へ更新
    
    * chore: package版をPWA版へ同期

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Yoshimi SMC FC Portal Service Worker
-const APP_VERSION = "20260719-8";
+const APP_VERSION = "20260719-9";
 const CACHE_PREFIX = "smc-portal-";
 const CACHE_NAME = CACHE_PREFIX + APP_VERSION;
 
