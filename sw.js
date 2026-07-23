10cfd2e380a1b2478dace326bf47dee9b0905ec7
yoshimismcfc-dotcom <yoshimi.smc.fc@gmail.com>
2026-07-23 17:36:35 +0900

    UI/UX改善: sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Yoshimi SMC FC Portal Service Worker
-const APP_VERSION = "20260721-05";
+const APP_VERSION = "20260723-01";
 const CACHE_PREFIX = "smc-portal-";
 const CACHE_NAME = CACHE_PREFIX + APP_VERSION;
 
