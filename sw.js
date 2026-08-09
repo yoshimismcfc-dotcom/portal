9180ee75942c20fb5d0a92c1112566297008a170
yoshimismcfc-dotcom <yoshimi.smc.fc@gmail.com>
2026-08-09 19:55:19 +0900

    大会要項を直接入力できる画面へ改善

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Yoshimi SMC FC Portal Service Worker
-const APP_VERSION = "20260809-13";
+const APP_VERSION = "20260809-14";
 const CACHE_PREFIX = "smc-portal-";
 const CACHE_NAME = CACHE_PREFIX + APP_VERSION;
 
