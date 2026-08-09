1eb124384340dc38b366fb4d9230bf2f909db400
yoshimismcfc-dotcom <yoshimi.smc.fc@gmail.com>
2026-08-09 14:46:42 +0900

    A4 PDF出力と印刷配色を修正

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Yoshimi SMC FC Portal Service Worker
-const APP_VERSION = "20260809-4";
+const APP_VERSION = "20260809-5";
 const CACHE_PREFIX = "smc-portal-";
 const CACHE_NAME = CACHE_PREFIX + APP_VERSION;
 
