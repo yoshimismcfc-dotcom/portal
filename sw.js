521370f1863ecdd199f9b9729bed6cafb6ccbf4a
yoshimismcfc-dotcom <yoshimi.smc.fc@gmail.com>
2026-08-22 15:39:24 +0900

    タスク画面の文字と操作ボタンを見やすく調整

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Yoshimi SMC FC Portal Service Worker
-const APP_VERSION = "20260816-41";
+const APP_VERSION = "20260822-1";
 const CACHE_PREFIX = "smc-portal-";
 const CACHE_NAME = CACHE_PREFIX + APP_VERSION;
 
